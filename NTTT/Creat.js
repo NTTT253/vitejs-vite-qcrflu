a1180939ee30a1dcc3e7ac59232a8b1fc56bd41d
NTTT253 <19021190@vnu.edu.vn>
2022-04-22 14:33:55 +0700

    Initial commit

---
+++ NTTT/Creat.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+
+const app = createApp({
+  /* root component options */
+  NTTT,
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-qcrflu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-qcrflu)
